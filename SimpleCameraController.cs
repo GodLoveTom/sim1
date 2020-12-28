1faf8f208d71ecd301b0634039aaa3203dc80417
GodLoveTom <993352272@qq.com>
2020-12-28 12:58:06 +0800

    first

--- SimpleCameraController.cs
+++ SimpleCameraController.cs
@@ -12,6 +12,7 @@ namespace UnityTemplateProjects
             public float x;
             public float y;
             public float z;
+            public float w;
 
             public void SetFromTransform(Transform t)
             {
